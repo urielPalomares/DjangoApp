097aea5bfe2d60a3ab8f38808431df445d676492
UrielPalomares <urieleliasg@gmail.com>
2017-11-07 13:01:01 -0600

    Agregar validacion en el form

--- src/boletin/views.py
+++ src/boletin/views.py
@@ -8,7 +8,11 @@ from django.shortcuts import render
 # Create your views here.
 
 def inicio(request):
-    form = RegForm()
+    form = RegForm(request.POST or None)
+    if form.is_valid():
+        form_data =  form.cleaned_data
+        print form_data.get("nombre")
+        print form_data.get("edad")
     context = {
         "el_form": form,
     }
