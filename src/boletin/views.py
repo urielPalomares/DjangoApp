0ad033b0c8cc6b85d193ff80947597b05bd3b1d5
UrielPalomares <urieleliasg@gmail.com>
2017-11-07 12:51:08 -0600

    Agregar angular y crear vista con formulario

--- src/boletin/views.py
+++ src/boletin/views.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from .forms import RegForm
+
 from django.shortcuts import render
 
 # Create your views here.
 
 def inicio(request):
-    return render(request, "inicio.html", {})
+    form = RegForm()
+    context = {
+        "el_form": form,
+    }
+    return render(request, "inicio.html", context)
